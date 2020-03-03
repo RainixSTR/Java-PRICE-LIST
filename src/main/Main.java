0e4c543af372c4884b849f21ff5e6317b0a13782
PORESTORANAM <spidy_max2@mail.ru>
2020-03-03 16:20:41 +0300

    Final Commit, add Tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,19 @@ package main;
 
 import pricelist.*;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         PriceList priceList = new PriceList();
-        priceList.addProduct(1, "hello", 4.2);
+        priceList.addProduct("hello", 4.2);
+        PriceList priceList1 = new PriceList(new HashMap<Integer, PriceList.Product>() {{
+            put(1, new PriceList.Product("1", 0));
+            put(2, new PriceList.Product("2", 0));
+        }});
+        System.out.println(priceList);
+        System.out.println(priceList1);
+
     }
 }
--- src/pricelist/PriceList.java
+++ src/pricelist/PriceList.java
@@ -1,31 +1,115 @@
 package pricelist;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class PriceList {
 
-    public class Product {
-        private final long id;
+    public static class Product {
         private String name;
         private double price;
 
-        private Product(long id, String name, double price) {
-            this.id = id;
+        public Product(String name, double price) {
             this.name = name;
             this.price = price;
         }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("NAME: ");
+            sb.append(name);
+            sb.append(", PRICE: ");
+            sb.append(price);
+            return sb.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Product product = (Product) o;
+            return Double.compare(product.price, price) == 0 &&
+                    Objects.equals(name, product.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, price);
+        }
     }
 
-    private List<Product> productList;
+    private Map<Integer, Product> productList = new HashMap<>();
+    private int lastID = 0;
+
+    public PriceList(Map<Integer, Product> list) {
+       productList.putAll(list);
+       int maxID = 0;
+       for (Integer id: productList.keySet()) {
+           if (maxID < id) maxID = id;
+       }
+       lastID = maxID + 1;
+    }
+
+    public PriceList() {
+    }
 
     public void addProduct(Product newProduct) {
-        productList.add(newProduct);
+        productList.put(lastID + 1, newProduct);
+        lastID++;
     }
 
-    public void addProduct(long id, String name, double price) {
-        productList.add(new Product(id, name, price));
+    public void addProduct(String name, double price) {
+        addProduct(new Product(name, price));
     }
 
+    public void setName(Integer id, String newName) {
+        productList.get(id).name = newName;
+    }
 
+    public void setPrice(Integer id, double newPrice) {
+        productList.get(id).price = newPrice;
+    }
+
+    public void removeProduct(Integer id) {
+        productList.remove(id);
+    }
+
+    public double getPrice(Integer id, int count) {
+        return productList.get(id).price * count;
+    }
+
+    public double getPrice(Integer id) {
+        return getPrice(id, 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof PriceList)) return false;
+        PriceList other = (PriceList) o;
+        return this.productList.equals(other.productList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productList);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Price List (SIZE = ");
+        sb.append(productList.size());
+        sb.append(") {\n");
+        for (Map.Entry<Integer, Product> entry: productList.entrySet()) {
+            sb.append("{ID: ");
+            sb.append(entry.getKey());
+            sb.append(", ");
+            sb.append(entry.getValue());
+            sb.append("}\n");
+        }
+        return sb.append("}").toString();
+    }
 }
